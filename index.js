2c10c5d5b7617954baeb2008b4365c1b6575e4f6
Daniel Vadil <dvadz@yahoo.com>
2022-01-02 23:00:23 -0500

    Added basic get routes

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const express = require("express");
+const res = require("express/lib/response");
 const app = express();
 
-app.use(() => {
-  console.log("We got a new request!");
+// app.use(() => {
+//   console.log("We got a new request!");
+// });
+
+app.get("/", (req, res) => {
+  res.send("Home page");
+});
+
+app.get("/about", (req, res) => {
+  res.send("About page");
+});
+
+app.get("/contact", (req, res) => {
+  res.send("Contact Us");
 });
 
 app.listen(3000, () => {
